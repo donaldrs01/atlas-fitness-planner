8b1d388bf82d87598f52c2a5bfc47ab4278c454e
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-07 11:34:42 -0500

    added error messages and check for matching passwords on registration

--- src/authentication.js
+++ src/authentication.js
@@ -7,12 +7,21 @@ signUpForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const email = document.getElementById("sign-up-email").value;
     const password = document.getElementById("sign-up-password").value;
+    const repeatPassword = document.getElementById("sign-up-password-repeat").value;
+    const signupErrorMessage = document.getElementById("sign-up-error");
+
+    // Check for password matching when registering
+    if (password !== repeatPassword) {
+        signupErrorMessage.textContent = "Passwords do not match. Please try again.";
+        return;
+    }
 
     try {
         const userCredential = await createUserWithEmailAndPassword(authorize, email, password);
         const user = userCredential.user;
         console.log("User signed up:", user);
         signUpForm.reset();
+        signupErrorMessage.textContent = ""
     } catch (error) {
         console.error("Error signing up:", error);
     }
@@ -24,7 +33,7 @@ loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const email = document.getElementById("login-email").value;
     const password = document.getElementById("login-password").value;
-    const errorMessage = document.getElementById("login-error");
+    const loginErrorMessage = document.getElementById("login-error");
 
     try {
         const userCredential = await signInWithEmailAndPassword(authorize, email, password);
@@ -32,10 +41,10 @@ loginForm.addEventListener("submit", async (e) => {
         console.log("User logged in:", user);
         loginForm.reset();
         // clear error message
-        errorMessage.textContent = ""
+        loginErrorMessage.textContent = ""
     } catch (error) {
         console.error("Error logging in:", error);
-        errorMessage.textContent = "Unrecognized login information. Please try again.";
+        loginErrorMessage.textContent = "Unrecognized login information. Please try again.";
     }
 });
 
